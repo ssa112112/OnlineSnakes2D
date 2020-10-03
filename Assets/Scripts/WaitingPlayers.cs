d29b5b6f284033cfa759788920908cd343275d11
ssa112112 <ssa112112@ya.ru>
2020-10-04 00:56:57 +0300

    Added waiting all players

---
+++ Assets/Scripts/WaitingPlayers.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaitingPlayers : MonoBehaviourPunCallbacks
+{
+     [SerializeField] Text connectedCountText;
+     [SerializeField] Text roomNumberText;
+
+     void Start()
+     {
+         roomNumberText.text += PhotonNetwork.CurrentRoom.Name;
+         UpdateConnectedCount();
+     }
+
+     public override void OnPlayerEnteredRoom(Player newPlayer) => UpdateConnectedCount();
+     
+     public override void OnPlayerLeftRoom(Player otherPlayer) => UpdateConnectedCount();
+
+     void UpdateConnectedCount()
+     {
+         byte countOfPlayersInRooms = PhotonNetwork.CurrentRoom.PlayerCount;
+         byte needPlayersInRoom = (byte) PhotonNetwork.CurrentRoom.CustomProperties[RoomOptionKeys.PlayersInRoom];
+         
+         connectedCountText.text =
+             $"{countOfPlayersInRooms} of {needPlayersInRoom} connected";
+         
+         if (countOfPlayersInRooms == needPlayersInRoom)
+            Destroy(gameObject);
+     }
+}
